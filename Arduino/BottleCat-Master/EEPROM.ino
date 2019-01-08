13f8cbc3d42b3b6d989210ccb8ef985fc5c27776
Reacher93 <reacher93@gmail.com>
2019-01-08 20:53:27 +0100

    Basis Funktion EEPROM
    V0.1, speichern von einer Variable in dem EEPROM

--- Arduino/BottleCat-Master/EEPROM.ino
+++ Arduino/BottleCat-Master/EEPROM.ino
@@ -3,3 +3,29 @@ Programm um remanete Variablen in der EEPROM zu speicher und wieder auszulesen
 */
 
 #include <EEPROM.h>
+
+int add = 0;        //Momentane Adresse, ab welchem Byte gelesen wird (Arduino Uno max. 512 bytes)
+
+void setup() {
+    
+    Serial.begin(9600);     //Starte Serial Monitor für die Anzeige der Variable
+
+}
+
+void loop() {
+
+    setE1();      //Funktion ausführen
+    byte val;       //Definiere die Variable val als Datentyp byte
+    val = EEPROM.read(add);     //Sreibt den Inhalt des EEPROMs in die Variable val
+    Serial.println(val);        //Gibt die ausgelesen Variable im Serial Monitor aus
+
+    delay(1000);
+
+}
+
+void setE1() {      //Funktion um Variable im 1ten Byte des EEPROM zu speichern
+
+    int val = 7;       //Test Variable gesetzt
+    EEPROM.write(add, val);     //Speichere Variable auf EEPROM
+
+}
