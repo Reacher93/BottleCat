35e6b09b28e538ca9a5413548660bcb04f42e571
Reacher93 <reacher93@gmail.com>
2019-01-08 21:26:49 +0100

    EEPROM float speichern
    V0.2 Test mit abspeichern von floats in der EEPROM

--- Arduino/BottleCat-Master/EEPROM.ino
+++ Arduino/BottleCat-Master/EEPROM.ino
@@ -15,9 +15,16 @@ void setup() {
 void loop() {
 
     setE1();      //Funktion ausführen
-    byte val;       //Definiere die Variable val als Datentyp byte
-    val = EEPROM.read(add);     //Sreibt den Inhalt des EEPROMs in die Variable val
-    Serial.println(val);        //Gibt die ausgelesen Variable im Serial Monitor aus
+    byte val1;       //Definiere die Variable val1 als Datentyp byte
+    val1 = EEPROM.read(add);     //Schreibt den Inhalt des 1ten Bytes des EEPROMs in die Variable
+    Serial.println(val1);        //Gibt die ausgelesen Variable im Serial Monitor aus
+
+    delay(1000);
+
+    setE2();        //Funktion ausführen
+    float val2;     //Definiere die Variable val2 als Datentyp float
+    val2 = EEPROM.get(add);     //Schreibt den Inhalt ab dem 2ten Byte des EEPROMs in die Variable
+    Serial.println(val2);       //Gibt die ausgelesen Variable im Serial Monitor aus
 
     delay(1000);
 
@@ -27,5 +34,13 @@ void setE1() {      //Funktion um Variable im 1ten Byte des EEPROM zu speichern
 
     int val = 7;       //Test Variable gesetzt
     EEPROM.write(add, val);     //Speichere Variable auf EEPROM
+
+}
+
+void setE2() {      //Funktion um grössere Daten ab 2ten Byte zu speichern
+    
+    float val = 22.5;      //Test Variable setzen
+    add = 1;     //Zum 2ten Byte springen für das speichern
+    EEPROM.put(add, val);        //speichert val in EEPROM
 
 }
