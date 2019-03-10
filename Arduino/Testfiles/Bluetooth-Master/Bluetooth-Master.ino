fbf2b90fbd8f2229fb785fccc5d7d1ca7519c754
Damian <reacher93@gmail.com>
2019-03-10 16:26:01 +0100

    Bluetooth, für esp32

--- Arduino/Testfiles/Bluetooth-Master/Bluetooth-Master.ino
+++ Arduino/Testfiles/Bluetooth-Master/Bluetooth-Master.ino
@@ -3,41 +3,49 @@
 /*int tx = 7; // pin 7 als tx definiert
 int rx = 8; // pin 8 als rx definiert -> Für das ESP32 Modul nicht benötigt*/
 
-BluetoothSerial BT;   // für EPS32 nicht benötigt(tx, rx); //Serialschnitstelle BT definiert
+BluetoothSerial BT;   //Serialschnitstelle BT definiert
 
-String var; //String var definiert
+void initBT(){    //Initialisierung vom Bluetooth
+  BT.begin("Bottle-Cat");   //starte Serialschnitstelle mit Bluetooth device name
+}
+
+String BTabfragen(){
+  String Vread;   //String Vread definiert
+  if (BT.available()) //BT abfrage
+  {
+    Vread = BT.readString();  //BT Wert wird in Variable eingelesen
+  }
+  return(Vread);
+}
+
+void BTsenden(String Vsend){
+  if (Serial.available())  //Wert aus Monitor abfragen
+  {
+    Vsend = Serial.readString();
+  }
+  BT.print(Vsend);
+}
 
 void setup()
 {
-BT.begin("Bottle-Cat"); //starte Serialschnitstelle mit Bluetooth device name
-Serial.begin(4800);
-Serial.println("Monitor läuft");
+  initBT();
+  
+  Serial.begin(4800);
+  Serial.println("Monitor läuft");
 }
 
 void loop()
 {
-if (BT.available()) //BT abfrage
-{
-    var = BT.readString();  //BT Wert wird in Variable eingelesen
-    
-    if (var == "1"){
+    if (BTabfragen() == "1"){
       Serial.println("Verbindung ok");
-      Serial.println(var);
+      Serial.println(BTabfragen());
     }
-    else if (var == "2"){ 
+    else if (BTabfragen() == "2"){ 
       Serial.println("Flasche wird geöffnet");
-      Serial.println(var);
+      Serial.println(BTabfragen());
     }
-    Serial.println(var);
-  }
+    Serial.println(BTabfragen());
 
 delay(500);
 
-//Für Test Monitoreingabe auf App anzeigen
-String Mvar;
-if (Serial.available())  //Wert aus Monitor abfragen
-  {
-  Mvar = Serial.readString();
-  }
-  BT.print(Mvar);
 }
