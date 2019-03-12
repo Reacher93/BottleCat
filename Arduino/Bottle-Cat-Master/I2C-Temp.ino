d43bdb9b7da7d6ca692cc2a54430c39a4187bc62
Damian <reacher93@gmail.com>
2019-03-12 21:42:06 +0100

    Masterfiles V1.0

--- Arduino/Bottle-Cat-Master/I2C-Temp.ino
+++ Arduino/Bottle-Cat-Master/I2C-Temp.ino
@@ -1,13 +1,14 @@
 #include <Wire.h>
 
 #define TempAdd 0x40        //Temperatursensor I2C Hex Address
-#define TempO 0x03       //Interne Adresse für Objekt Temperatur
+#define TempO 0x03          //Interne Adresse für Objekt Temperatur
+#define TempR 0x01          // Board Temp. für Raum Temp.
 
 void initI2C(){
   Wire.begin();       //Starte Wire library
   }
 
-float readTemp(void)    //Funktion für das Auslesen der Temperatur
+float readTempBtl(void)    //Funktion für das Auslesen der Temperatur
 {
   float OT;         //Variable für Objekt Temperatur
   
@@ -24,6 +25,23 @@ float readTemp(void)    //Funktion für das Auslesen der Temperatur
     return(OT);
 }
 
+float readTempRoom(void)    //Funktion für das Auslesen der Temperatur
+{
+  float RT;         //Variable für Raum Temperatur
+  
+  Wire.beginTransmission(TempAdd);        //Starte Kommunikation zum Temperatursensor
+  Wire.write(TempR);     //Abfrage der Objekt Temperatur
+      
+  Wire.endTransmission();     //Beendet die Kommunikation
+
+  Wire.requestFrom(TempAdd,1);        //Abfrage der übertragenen Daten von den zwei Limit Temperaturen (?2 Daten oder 20 bits?)
+
+  if(Wire.available()<=1){        //Nachfragen ob die zwei passenden Daten verfügbar sind und in die ensprechenden Varianlen einlesen (OT)
+    RT = Wire.read();
+    }
+    return(RT);
+}
+
 /*
 void setup(){
 
@@ -35,7 +53,7 @@ void setup(){
 void loop(){
   
   Serial.print("Die gemessene Temperatur ist: ");
-  Serial.println(readTemp());
+  Serial.println(readTempBtl());
   delay(500);
 
 }
